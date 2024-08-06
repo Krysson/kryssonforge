74cd6b0445bb038fa32db604b706fb5676b46491
Krysson <mgulla@krysson.com>
2024-08-06 16:27:10 -0400

    feat: Add Hero component to homepage
    The commit adds the Hero component to the homepage, improving the visual appeal and user experience. The Hero component includes a catchy headline, a description of the product, and call-to-action buttons. This enhancement enhances the overall appearance and engagement of the homepage.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import Hero from '@/components/hero'
 // import NavigationMenu from '@/components/NavigationMenu'
 
 export default function Home() {
   return (
     <div className='page-container'>
-      <p className='text-4xl'>CLEAN START</p>
+      <Hero />
 
       <div className='content'>{/* Your page content goes here */}</div>
     </div>
---
+++ src/components/hero.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    /* Hero Section */
+
+    <section className='pt-24 pb-20 px-4 md:px-0'>
+      {' '}
+      {/* Increased top padding to account for fixed header */}
+      <div className='container mx-auto flex flex-col md:flex-row items-center'>
+        <div className='md:w-1/2 mb-10 md:mb-0'>
+          <h1 className='text-4xl md:text-6xl font-bold mb-6'>
+            Everything all together, in one place, at the same time.
+          </h1>
+          <p className='text-xl mb-8'>
+            Improve project workflow and collaboration. Complete more project on time and under
+            budget.
+          </p>
+          <div className='flex space-x-4'>
+            <Button className='bg-blue-600 hover:bg-blue-700 text-white'>Get Started</Button>
+            <Button variant='outline'>Download IDE App</Button>
+          </div>
+        </div>
+        <div className='md:w-1/2'>
+          <div className='aspect-w-16 aspect-h-9 bg-gray-200 rounded-lg'>
+            {/* Placeholder for your image */}
+            <Image
+              src={'/../../public/images/KFTrans.png'}
+              width={100}
+              height={100}
+              alt='KryssonForge Logo'
+            />
+            <div className='flex items-center justify-center h-full text-gray-500'>
+              Placeholder for Image
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
