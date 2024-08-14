945268e4087b841600841ed9a78ef366be0bc0ed
Krysson <mgulla@krysson.com>
2024-08-14 16:19:49 -0400

    feat: Add dhtmlx-gantt and dayjs dependencies
    The dhtmlx-gantt and dayjs dependencies have been added to the project. This enables the integration of a Gantt chart component and a lightweight date manipulation library, respectively.

---
+++ src/app/schedule/page.jsx
@@ -0,0 +1,10 @@
+import ProjectScheduler from '@/components/ProjectScheduler';
+
+export default function SchedulerPage() {
+  return (
+    <div>
+      <h1>Project Gantt Chart</h1>
+      <ProjectScheduler />
+    </div>
+  );
+}
