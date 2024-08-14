f865c0019d6336b250d204b464f70977e84572eb
Krysson <mgulla@krysson.com>
2024-08-14 09:12:48 -0400

    chore: trying to fix mongodb connection issue. Edited mongoose.js , edited schema.prisma

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -12,7 +12,7 @@ export const connect = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: 'KryssonForge',
+      dbName: 'kryssonforge',
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
